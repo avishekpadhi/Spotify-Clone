a9f0318ff4ed9a67e832ba14d178092f5e764f76
Abhishek <abhishekpadhi69@gmail.com>
2022-10-16 11:12:02 +0530

    First commit

--- spotify.js
+++ spotify.js
@@ -1,48 +1,80 @@
-console.log("Welcome to Spotify");
+console.log("Hello world")
+let audioElement= new Audio('1.mp3')
+let masterPlay=document.getElementById('masterPlay')
+let myProgressBar=document.getElementById('myProgressBar')
+let songIndex=0
+let gif=document.getElementById('gif')
+let songList=Array.from(document.getElementsByClassName('songItem'))
 
-let songindex=0;
-let audioelement= new Audio('1.mp3');
-let masterplay=document.getElementById('masterplay');
-let myprogressbar=document.getElementById('myprogressbar');
-let gif=document.getElementById('gif');  
 
 let songs=[
-    {songName: "Hybrid Theory", filePath: "song/1.mp3",coverPath:"covers/1.jpg"},
-    {songName: "Meteora", filePath: "song/2.mp3",coverPath:"covers/2.jpg"},
-    {songName: "Castle of glass", filePath: "song/3.mp3",coverPath:"covers/3.jpg"},
-    {songName: "Numb", filePath: "song/4.mp3",coverPath:"covers/4.jpg"},
-    {songName: "Breaking the habit", filePath: "song/5.mp3",coverPath:"covers/5.jpg"},
-    {songName: "Lost in the echo", filePath: "song/6.mp3",coverPath:"covers/6.jpg"},
-    {songName: "", filePath: "song/6.mp3",coverPath:"covers/6.jpg"},
-    {songName: "Hybrid Theory", filePath: "song/6.mp3",coverPath:"covers/6.jpg"}
+    {songName:"Heavy",filePath: "songs/1.mp3",coverPath:"covers/1.jpg"},
+    {songName:"Breaking the habit",filePath: "songs/2.mp3",coverPath:"covers/2.jpg"},
+    {songName:"New Divide",filePath: "songs/3.mp3",coverPath:"covers/3.jpg"},
+    {songName:"Powerless",filePath: "songs/4.mp3",coverPath:"covers/4.jpg"},
+    {songName:"Numb",filePath: "songs/5.mp3",coverPath:"covers/5.jpg"},
+    {songName:"Castle of Glass",filePath: "songs/6.mp3",coverPath:"covers/6.jpg"},
+    {songName:"Leave Out all the rest",filePath: "songs/7.mp3",coverPath:"covers/7.jpg"},
+    {songName:"Burn it down",filePath: "songs/8.mp3",coverPath:"covers/8.jpg"},
+    {songName:"Somewhere I belong",filePath: "songs/9.mp3",coverPath:"covers/9.jpg"},
+    {songName:"Lost in the echo",filePath: "songs/10.mp3",coverPath:"covers/10.jpg"},
+
 ]
 
-masterplay.addEventListener('click',()=>{
-    if(audioelement.paused || audioelement.currentTime<=0){
-        audioelement.play();
-        masterplay.classList.remove('fa-play-circle');
-        masterplay.classList.add('fa-pause-circle');
+
+
+audioElement.addEventListener('timeupdate',()=>{
+    // console.log("timeupdate")
+    progress= parseInt((audioElement.currentTime/audioElement.duration)*100);
+    // console.log(progress)
+    myProgressBar.value=progress
+
+})
+
+myProgressBar.addEventListener("change",()=>{
+    audioElement.currentTime=myProgressBar.value * audioElement.duration/100
+
+
+
+
+
+})
+
+masterPlay.addEventListener ('click', ()=> {
+    if (audioElement.paused || audioElement.currentTime <= 0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play')
+        masterPlay.classList.add('fa-circle-pause')
         gif.style.opacity = 1;
-    }else{
-        audioelement.pause();
-        masterplay.classList.remove('fa-pause-circle');
-        masterplay.classList.add('fa-play-circle');
+    }
+    else{
+        audioElement.pause()
+        masterPlay.classList.remove('fa-circle-pause')
+        masterPlay.classList.add('fa-circle-play')
         gif.style.opacity = 0;
     }
 })
-audioelement.addEventListener('timeupdate',()=>{
-    // console.log('timeupdate');
-    progress = parseInt((audioelement.currentTime/audioelement.duration)*100);
-    // console.log(progress);
-    myprogressbar.value = progress;
+ 
+
+// songList.forEach((element,i) => {
+//     // console.log(element,i);
+//     element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+//     element.getElementsByTagName("span")[0].innerText = songs[i].songName;
+//     // element.
+
+// })
+// // console.log(songList)
 
 
-})
-myprogressbar.addEventListener('change',()=>{
-    audioelement.currentTime=myprogressbar.value*audioelement.duration/100;
-})
+// Array.from(document.getElementsByClassName("songPlay")).forEach((element)=>{
+//     element.addEventListener('click',(e)=>{
+//         console.log(e)
+//         index=parseInt()
+//         audioElement.src=`songs/${index}.mp3`
+//         audioElement.play()
+
+//     })
 
 
-
-
+// })
 
